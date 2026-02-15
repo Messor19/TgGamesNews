a6d0655deb16e66330ba4a5cfaeaae40e089ebea
Messor <daniel.bozhko1903@gmail.com>
2026-02-15 19:56:06 +0300

    Рабочий код

--- TgGamesNews.py
+++ TgGamesNews.py
@@ -6,16 +6,17 @@ load_dotenv()
 
 API_ID = int(os.getenv('API_ID'))
 API_HASH = os.getenv('API_HASH')
-MY_CHANNEL = os.getenv('MY_CHANNEL')
-CHANNELS = os.getenv('CHANNELS')
+MY_CHANNEL = int(os.getenv('MY_CHANNEL'))
+CHANNELS = os.getenv('CHANNELS').split(',')
+
+for i in range(len(CHANNELS)):
+    CHANNELS[i] = int(CHANNELS[i])
 
 client = TelegramClient("Новости", API_ID, API_HASH)
-
 
 @client.on(events.NewMessage(CHANNELS))
 async def handler(event):
     await event.forward_to(MY_CHANNEL)
-
 
 client.start()
 client.run_until_disconnected()
